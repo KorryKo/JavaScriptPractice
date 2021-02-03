69eccd46e505c639a133e2f8dd2f82d9f1de9350
Marina <maryaleks@mail.ru>
2021-02-03 13:47:17 +0300

    add solution for the second homework

---
+++ Homework2/scriptTask5.js
@@ -0,0 +1,24 @@
+//  Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
+
+var a = 8,
+    b = 5;
+
+function plus(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+console.log(plus(a, b));
+console.log(minus(a, b));
+console.log(division(a, b));
+console.log(multiplication(a, b));
